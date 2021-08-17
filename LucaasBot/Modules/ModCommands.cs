e9880ffdb7dad6a1f6ebb18863484ee703692832
quin lynch <lynchquin@gmail.com>
2021-08-17 01:28:20 -0300

    Disable move command

--- LucaasBot/Modules/ModCommands.cs
+++ LucaasBot/Modules/ModCommands.cs
@@ -306,7 +306,7 @@ namespace LucaasBotBeta.Modules
             await ReplyAsync(embed: embed, messageReference: Context.Message.GetReference());
         }
 
-        [Command("move")]
+        //[Command("move")]
         public async Task MoveUserVoice(SocketGuildUser userAccount = null, SocketVoiceChannel channel = null)
         {
             if(Context.User is not SocketGuildUser user)
